f0799f6d2406c3aebbfcf5132a3dd90fb1e7706e
Sylvain Gournay <sylvain.gournay@aworldforus.com>
2026-03-10 10:42:55 +0100

    feat: use Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-
+// Vercel Analytics
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
