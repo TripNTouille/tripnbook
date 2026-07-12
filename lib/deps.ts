cfef118ac5251879de4e51d29c6b04b1976335e5
Sylvain Gournay <sylvain.gournay@aworldforus.com>
2026-07-12 15:15:53 +0200

    chore: upgrade stripe lib to v22

--- lib/deps.ts
+++ lib/deps.ts
@@ -5,7 +5,7 @@ import { sendBookingConfirmation } from "@/lib/email"
 import type { CalendarDeps, EmailDeps } from "@/lib/checkout"
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-03-25.dahlia",
+  apiVersion: "2026-06-24.dahlia",
 })
 
 export const calendar: CalendarDeps = {
